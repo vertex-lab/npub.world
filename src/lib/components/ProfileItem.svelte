342cb202d181a33e71b350819a0a04e199564ebf
pippellia <pippellia@gmail.com>
2025-08-01 15:00:25 +0200

    added hovering to profile item

--- src/lib/components/ProfileItem.svelte
+++ src/lib/components/ProfileItem.svelte
@@ -15,35 +15,40 @@
 </a>
 
 <style>
-  a {
-    color: var(--primary-color);
-    text-decoration: none;
-  }
+a {
+color: var(--primary-color);
+text-decoration: none;
+}
 
-  .profile-item {
-    display: flex;
-    align-items: center;
-    padding: 5px;
-    border-radius: 8px;
-    transition: background-color 0.2s;
-  }
+.profile-item {
+display: flex;
+align-items: center;
+padding: 5px;
+border-radius: 8px;
+transition: background-color 0.2s;
+}
 
-  .profile-info {
-    overflow: hidden;
-  }
+.profile-item:hover {
+background-color: var(--highlight-color);
+cursor: pointer;
+}
 
-  .profile-name {
-    font-weight: 500;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
+.profile-info {
+overflow: hidden;
+}
 
-  .profile-nip05 {
-    font-size: 0.75rem;
-    color: var(--light-text);
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
+.profile-name {
+font-weight: 500;
+white-space: nowrap;
+overflow: hidden;
+text-overflow: ellipsis;
+}
+
+.profile-nip05 {
+font-size: 0.75rem;
+color: var(--light-text);
+white-space: nowrap;
+overflow: hidden;
+text-overflow: ellipsis;
+}
 </style>

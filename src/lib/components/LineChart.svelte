9185c81816ff06a72a03d9184fe390feb90d574d
pippellia-btc <pippellia@gmail.com>
2025-09-25 20:56:20 +0200

    making chart responsive to theme dark or light mode

--- src/lib/components/LineChart.svelte
+++ src/lib/components/LineChart.svelte
@@ -1,34 +1,44 @@
 <script>
-/**
-  * A single dataset for a chart.
-  * 
-  * @typedef {Object} Dataset
-  * @property {string} label - The name of the dataset (shown in the legend).
-  * @property {Array<Point>} points - An array of data points for the dataset.
-*/
- 
- /**
-  * A single point in a dataset.
-  * 
-  * @typedef {Object} Point
-  * @property {string|number} x - The x-axis value of the point.
-  * @property {number} y - The y-axis value of the point.
-*/
-
+  /**
+    * A single dataset for a chart.
+    * 
+    * @typedef {Object} Dataset
+    * @property {string} label - The name of the dataset (shown in the legend).
+    * @property {Array<Point>} points - An array of data points for the dataset.
+  */
+  
+  /**
+    * A single point in a dataset.
+    * 
+    * @typedef {Object} Point
+    * @property {string|number} x - The x-axis value of the point.
+    * @property {number} y - The y-axis value of the point.
+  */
   import { browser } from "$app/environment";
   import { onMount, tick } from "svelte";
   import { Chart, layouts, registerables } from "chart.js";
+  import { theme } from "$lib/theme.svelte";
 
   let { datasets, title } = $props();
+  let chart;
   let canvas;
 
-  const formatter = new Intl.NumberFormat("en", {
-    notation: "compact",
-    maximumFractionDigits: 0,
-  });
+  // change these colors if the correspondent colors in shared.css change
+  const color = {
+    light: {
+      text: "#333",
+      grid: "#0000001a",
+    },
+
+    dark: {
+      text: "#e1e1e1",
+      grid: "#ffffff1a",
+    },
+  }
 
   Chart.register(...registerables);
   Chart.defaults.font.family = "Ubuntu";
+  Chart.defaults.color = theme.isDark ? color.dark.text : color.light.text;
 
   const data = {
     datasets: datasets.map(d => ({
@@ -38,6 +48,11 @@
       tension: 0.1
     })),
   };
+
+  const formatter = new Intl.NumberFormat("en", {
+    notation: "compact",
+    maximumFractionDigits: 0,
+  });
 
   const options = {
     aspectRatio: 1.5,
@@ -53,8 +68,9 @@
     scales: {
       x: {
         type: 'category',
-        ticks: {
-          padding: 10, 
+
+        grid: {
+          color: theme.isDark ? color.dark.grid : color.light.grid,
         }
       },
 
@@ -64,13 +80,29 @@
           callback: function(value) {
             return formatter.format(value);
           },
+        },
+
+        grid: {
+          color: theme.isDark ? color.dark.grid : color.light.grid,
         }
-      }
+      },
     }
 };
 
   onMount(() => {
-    new Chart(canvas, { type: "line", data: data, options: options});
+    chart = new Chart(canvas, { type: "line", data: data, options: options});
+  });
+
+  $effect(() => {
+    if (chart) {
+      chart.options.plugins.legend.labels.color = theme.isDark ? color.dark.text : color.light.text;
+      chart.options.scales.x.ticks.color = theme.isDark ? color.dark.text : color.light.text;
+      chart.options.scales.y.ticks.color = theme.isDark ? color.dark.text : color.light.text;
+
+      chart.options.scales.x.grid.color = theme.isDark ? color.dark.grid : color.light.grid;
+      chart.options.scales.y.grid.color = theme.isDark ? color.dark.grid : color.light.grid;
+      chart.update();
+    }
   });
 </script>
 

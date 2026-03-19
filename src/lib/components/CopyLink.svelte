629a0ac7d6e6d02f1d70ae5791b3082cb9e43a50
pippellia-btc <pippellia@gmail.com>
2026-03-19 16:45:31 +0100

    changed link icon to lucide link

--- src/lib/components/CopyLink.svelte
+++ src/lib/components/CopyLink.svelte
@@ -12,8 +12,8 @@
       .catch(err => console.error('Failed to copy text:', err));
   }
 
-  $effect(() => { 
-      copied = false; 
+  $effect(() => {
+      copied = false;
   });
 </script>
 
@@ -29,19 +29,20 @@
     {#if copied}
       ✓
     {:else}
-      <svg
-        width="18"
-        height="18"
-        viewBox="-2.4 -2.4 28.8 28.8"
-        fill="none"
+    <svg
         xmlns="http://www.w3.org/2000/svg"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-      >
-        <path d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212"/>
-        <path d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373"/>
-      </svg>
+        width="16px" height="16px" viewBox="0 0 24 24">
+            <title xmlns="">link</title>
+            <g
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2">
+                    <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/>
+                    <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/>
+            </g>
+        </svg>
     {/if}
   </span>
 </button>
@@ -53,7 +54,7 @@
     justify-content: space-evenly;
     height: 40px;
     width: 90px;
-    
+
     border-radius: 8px;
     cursor: pointer;
     user-select: none;
